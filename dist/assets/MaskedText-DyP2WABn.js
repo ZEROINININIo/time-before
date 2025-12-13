4ce55508f5ff422aa5f265cdfe66ff26b7281a13
zeropoint <84187741+ZEROINININIo@users.noreply.github.com>
2025-12-13 19:02:51 +0800

    Add files via upload

---
+++ dist/assets/MaskedText-DyP2WABn.js
@@ -0,0 +1,4 @@
+import{r,j as o}from"./index-BImyePHj.js";const n=({children:t})=>{const[e,s]=r.useState(!1);return o.jsx("span",{onClick:a=>{a.stopPropagation(),s(!e)},className:`
+        px-1 mx-0.5 rounded-sm select-none cursor-help transition-all duration-300 inline-block align-middle
+        ${e?"bg-ash-light text-ash-black":"bg-ash-gray text-transparent hover:text-ash-black hover:bg-ash-light"}
+      `,title:"REDACTED_DATA [CLICK TO REVEAL]",children:t})};export{n as M};
